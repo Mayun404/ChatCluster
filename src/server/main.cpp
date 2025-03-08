444bc2d142b65695d991c396d0cbde30caecc2c1
jxnu <1819127584@qq.com>
2025-03-08 12:31:27 +0800

    ChatCluster

---
+++ src/server/main.cpp
@@ -0,0 +1,62 @@
+#include "chatserver.hpp"
+#include "chatservice.hpp"
+#include <iostream>
+#include <signal.h>
+using namespace std;
+
+// 处理服务器ctrl+c结束后，重置user的状态信息
+void resetHandler(int)
+{
+    ChatService::instance()->reset();
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3)
+    {
+        cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
+        exit(-1);
+    }
+
+    // 解析通过命令行参数传递的ip和port
+    char *ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    signal(SIGINT, resetHandler);
+    EventLoop loop;
+    InetAddress addr(ip, port);
+    ChatServer server(&loop, addr, "ChatServer");
+    signal(SIGINT, resetHandler);
+
+    server.start();
+    loop.loop();
+    return 0;
+}
+// {"msgid":1,"id":13,"password":"123456"}
+// {"msgid":1,"id":22,"password":"11"}
+// {"msgid":1,"name":"zhang san","password":"123456"}
+// {"id":13,"to":22,"msg":"kk","msgid":6}
+// int main(int argc, char **argv)
+// {
+//     if (argc < 3)
+//     {
+//         cerr << "command invalid! example: ./ChatServer 127.0.0.1 6000" << endl;
+//         exit(-1);
+//     }
+
+//     // 解析通过命令行参数传递的ip和port
+//     // char *ip = argv[1];
+//     // uint16_t port = atoi(argv[2]);
+
+//     // signal(SIGINT, resetHandler);
+
+//     EventLoop loop;
+//     InetAddress addr(ip, port);
+//     ChatServer server(&loop, addr, "ChatServer");
+
+//     server.start();
+//     loop.loop();
+
+//     return 0;
+// }
